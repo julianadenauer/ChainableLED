a12dd48e3ec691598a448f15069dc4be8ecb3ca1
julianadenauer <github@julianadenauer.de>
2016-11-17 11:38:15 +0100

    added example

---
+++ firmware/examples/P9813-example.ino
@@ -0,0 +1,21 @@
+#include "P9813.h"
+
+#define NUM_LEDS  1
+ChainableLED leds(D4, D5, NUM_LEDS);
+
+// This routine runs only once upon reset
+void setup()
+{
+  leds.setColorRGB(0, 0, 0, 0);                 // turn down the LED
+}
+
+// This routine loops forever
+void loop()
+{
+  leds.setColorRGB(0, 255, 0, 0);                 // make the LED red
+  delay(500);
+  leds.setColorRGB(0, 0, 255, 0);                 // make the LED green
+  delay(500);
+  leds.setColorRGB(0, 0, 0, 255);                 // make the LED blue
+  delay(500);
+}
